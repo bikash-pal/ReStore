4cffb112d313ae994fb93c7644c042e017e479ea
bikas <bikas@WOODY>
2023-06-18 00:17:10 +0530

    Add project files.

---
+++ API/Migrations/20230616155946_QuantityInClass.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class QuantityInClass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "QuantityOfStock",
+                table: "Products",
+                newName: "QuantityInStock");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "QuantityInStock",
+                table: "Products",
+                newName: "QuantityOfStock");
+        }
+    }
+}
