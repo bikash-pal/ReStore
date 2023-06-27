77495a6eb742c7e731eebc63915e166fb17bd6ca
bikash-pal <bpal13577@gmail.com>
2023-06-27 23:09:15 +0530

    basic redux implemented for learning

--- client/src/features/contact/ContactPage.tsx
+++ client/src/features/contact/ContactPage.tsx
@@ -1,9 +1,36 @@
-import { Typography } from "@mui/material";
+import { Button, ButtonGroup, Typography } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  CounterState,
+  DECREMENT_COUNTER,
+  INCREMENT_COUNTER,
+  decrement,
+  increment,
+} from "./counterReducer";
 
 export default function ContactPage() {
+  const dispatch = useDispatch();
+  const { data, title } = useSelector((state: CounterState) => state);
   return (
     <>
       <Typography variant="h2">Contact Page</Typography>
+      {data}-{title}
+      <ButtonGroup>
+        <Button
+          onClick={() => dispatch(increment())}
+          variant="contained"
+          color="error"
+        >
+          increment
+        </Button>
+        <Button
+          onClick={() => dispatch(decrement())}
+          variant="contained"
+          color="primary"
+        >
+          Decrement
+        </Button>
+      </ButtonGroup>
     </>
   );
 }
